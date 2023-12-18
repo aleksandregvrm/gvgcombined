e68ba7142076b710105558b7501e91a3a3c79efa
aleksandregvrm <aleksandregvrm@gmail.com>
2023-12-18 15:37:56 +0400

    first

---
+++ public-front/src/facebook-pixel/FacebookFunctions.jsx
@@ -0,0 +1,18 @@
+export const landingPageEvent = () => {
+    fbq("trackCustom", "PageViewHome");
+}
+export const vendingPageEvent = () => {
+    fbq("trackCustom", "PageViewVending");
+}
+export const coffeePageEvent = () => {
+    fbq("trackCustom","PageViewCoffee");
+}
+export const snackPageEvent = () => {
+    fbq("trackCustom","PageViewSnacks");
+}
+export const servicePageEvent = () => {
+    fbq("trackCustom","PageViewService");
+}
+export const detailsEvent = (category,name) => {
+    fbq("trackCustom", "interested", { content_type: category, content_name : name });
+}
